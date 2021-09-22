bede952c22206b743f26345eb0b29698666f9347
Louna <louna.bordet@gmail.com>
2021-09-22 16:15:29 +0200

    crea_louna

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
     name: "Antonio",
     component: () => import("../views/Antonio.vue"),
   },
+  {
+    path: "/louna",
+    name: "Louna",
+    component: () => import("../views/Louna.vue"),
+  },
 ];
 
 const router = createRouter({

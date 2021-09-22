3db6dc7e86306f5c8be0212e8edac6f286d95d6a
Braunschweig Malo <malo.braunschweig@live.fr>
2021-09-22 17:07:56 +0200

    quick fix

--- tests/e2e/specs/malo.spec.js
+++ tests/e2e/specs/malo.spec.js
@@ -1,10 +1,5 @@
 
 describe("Scenarios for page Malo", () => {
-    it("Visits page /Malo", () => {
-      cy.visit("http://localhost:8080/");
-      cy.get('[href="/malo"]').click();
-      cy.url().should("include", "/malo");
-    });
   
     it("Check if first project displayed is Autodistribution", () => {
       cy.visit("http://localhost:8080/malo");
@@ -13,12 +8,12 @@ describe("Scenarios for page Malo", () => {
 
     });
 
-   /* it("Check if second project displayed is not Autodistribution", () => {
+it("Check if second project displayed is not Autodistribution", () => {
         cy.visit("http://localhost:8080/malo");
     
         cy.get('[class="projects"]').not("have.text", "Autodistribution Web APP  Click to Shuffle");
   
-      });*/
+      });
 
   
   });
